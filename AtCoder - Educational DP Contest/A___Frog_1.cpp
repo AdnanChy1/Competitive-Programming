e518c5bc035ee41b2f2fdcf467c3317c678c0ad4
Chowdhury Adnan Raza <109912116+AdnanChy1@users.noreply.github.com>
2024-07-31 16:54:46 +0600

    Add files via upload

---
+++ AtCoder - Educational DP Contest/A___Frog_1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long int
+#define vv vector<long long>
+#define mpp map<long long,long long>
+#define ss set<long long>
+#define vp vector<pair<long long, long long>>
+#define MOD 1000000007
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define all_v v.begin(),v.end()
+#define all_a a.begin(),a.end()
+#define all_n for(int i=0;i<n;i++)
+void requisite(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+}
+int main() {
+    requisite();
+    ll n;
+    cin>>n;
+    vv v(n);
+    all_n{
+        cin>>v[i];
+    }
+    vv dp(n,INT_MAX);
+    dp[0]=0;
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = i+1; j <n ; ++j) {
+            dp[j]= min(dp[j], abs(v[j]-v[i])+dp[i]);
+        }
+    }
+    cout<<dp[n-1]<<endl;
+    return 0;
+}
